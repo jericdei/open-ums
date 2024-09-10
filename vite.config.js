5aa9cd5a5208b29369355822ea9cfda074a278d6
Jeric June Logan <loganjeric25@gmail.com>
2024-09-10 21:58:01 +0800

    create docker service for postgres db

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,18 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.tsx',
-            refresh: true,
-        }),
-        react(),
-    ],
+  plugins: [
+    laravel({
+      input: 'resources/js/app.tsx',
+      refresh: true,
+    }),
+    react(),
+  ],
+  server: {
+    host: "0.0.0.0",
+    hmr: {
+      host: "localhost",
+      clientPort: 5174, // The exposed port of your `app` service
+    },
+  },
 });
